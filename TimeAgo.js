752caee3491a24ffca3a8ba9da6bba9a78f6be27
cdelouvencourt <c.delouvencourt@nocturne.app>
2018-11-18 14:12:45 +0100

    update ago style

--- TimeAgo.js
+++ TimeAgo.js
@@ -41,7 +41,7 @@ export default class TimeAgo extends Component {
     const { time, hideAgo } = this.props;
     return (
       <Text {...this.props}>
-        {moment(time).fromNow(hideAgo)}
+        {moment(time).fromNow(hideAgo).bold().toLocaleUpperCase()}
       </Text>
     );
   }
